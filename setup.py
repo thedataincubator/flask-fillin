482a596d14e891a288267105e374bc341a31fd1e
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2016-03-27 20:54:00 -0400

    incrementing version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='Flask-fillin',
-    version='0.3',
+    version='0.4',
     url='http://github.com/jarus/flask-fillin/',
     license='BSD',
     author='Christoph Heer',
